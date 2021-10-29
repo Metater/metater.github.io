b19fdaa0160d8f577e57bd18d249e3a745afc743
Connor <61102529+Metater@users.noreply.github.com>
2021-10-29 17:56:10 -0400

    Deployed

--- metater.github.io/deploy.bat
+++
@@ -1,4 +0,0 @@
-
-npm run build
-del "E:\Projects\Visual Studio\metater.github.io\docs" /s /f /q
-xcopy /s "E:\Projects\Visual Studio\metater.github.io\metater.github.io\build" "E:\Projects\Visual Studio\metater.github.io\docs"
