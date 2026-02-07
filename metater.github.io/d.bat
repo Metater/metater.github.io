b5a9fcce12186aac4e565658b8608d8eb7cf6f34
Connor Myers <61102529+Metater@users.noreply.github.com>
2026-02-07 15:21:25 -0500

    remove

--- metater.github.io/d.bat
+++
@@ -1,3 +0,0 @@
-CALL build.bat
-del "E:\Projects\Visual Studio\metater.github.io\docs" /s /f /q
-xcopy /s /f /q /y "E:\Projects\Visual Studio\metater.github.io\metater.github.io\build" "E:\Projects\Visual Studio\metater.github.io\docs"
